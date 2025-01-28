7d0984d4b0ceab892ef19d486607dd31dd33ed8b
Diyorbek G'aybullayev <gaybullayevdiyorbek2001@gmail.com>
2025-01-28 13:14:58 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,7 @@ const App = () => {
       <Quality />
       <Logistic />
       <Contact />
-      <Footer />
-
+      <Footer /> 
       {/* <Swiper/> */}
       {/* <Services /> */}
       {/* <Form /> */}
